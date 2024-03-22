57c921053768be39d54af3137e91638ecca5f435
wschung1113 <wschung1113@gmail.com>
2024-03-22 17:31:50 +0900

    24.3.22) - wrote module to convert creditcard_fraud.arff file to csv file and save

--- creditcard_fraud_data_to_csv.py
+++ creditcard_fraud_data_to_csv.py
@@ -15,5 +15,9 @@ df = pd.DataFrame(data)
 # If you want to convert attribute names to column names
 df.columns = meta.names()
 
+df['Class'] = df['Class'].apply(lambda x: int.from_bytes(x, byteorder='big')) - 48 # convert 48 --> 0, 49 --> 1
+
+df_dropped = df.drop(columns='Time', axis=1)
+
 # Save DataFrame to CSV file
-df.to_csv(PATH + '/creditcard_fraud.csv', index=False)
+df_dropped.to_csv(PATH + '/creditcard_fraud.csv', index=False)
